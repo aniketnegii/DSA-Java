2426b7923999f3eef680523397f341534de1890e
aniketnegii <aniket.negi2112@gmail.com>
2021-09-21 18:04:34 +0530

    PeakIndexInAMountainArray

--- Binary Search/src/com/aniket/PeakIndex.java
+++ Binary Search/src/com/aniket/PeakIndex.java
@@ -7,7 +7,7 @@ public class PeakIndex {
     }
 
     static int findPeak(int[] nums){
-        int start = 0, end = nums.length, mid;
+        int start = 0, end = nums.length - 1, mid;
         while( start < end){
             mid = start + (end - start)/2;
             if( nums[mid] < nums[mid+1]) start = mid+1;
