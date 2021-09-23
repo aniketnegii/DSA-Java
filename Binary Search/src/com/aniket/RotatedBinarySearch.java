5b791b32f3c0a893a36da2ec6a26b9c3e4fee4a3
aniketnegii <aniket.negi2112@gmail.com>
2021-09-23 22:06:28 +0530

    RotatedBinarySearch

--- Binary Search/src/com/aniket/RotatedBinarySearch.java
+++ Binary Search/src/com/aniket/RotatedBinarySearch.java
@@ -48,4 +48,35 @@ public class RotatedBinarySearch {
         }
         return -1;
     }
+
+    static int findRotationPointInDuplicate(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+        while( start <= end){
+            mid = start + (end - start)/2;
+            if( mid < end && arr[mid] > arr[mid +1]){
+                return mid;
+            }
+            if( mid>start && arr[mid] < arr[mid -1]){
+                return mid-1;
+            }
+            if( mid == start && mid == end){
+                if(arr[start] > arr[start + 1]){
+                    return start;
+                }
+                start++;
+                if( arr[end] < arr[end - 1]){
+                    return end;
+                }
+                end --;
+            }
+            else if( arr[start] < arr[mid] || (arr[mid] == arr[start] && arr[mid] > arr[end])){
+                start = mid+1;
+            } else {
+                end = mid -1;
+            }
+        }
+        return -1;
+    }
 }
