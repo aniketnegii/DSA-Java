988683650074414c537cbc6de371dad453c8ca2c
aniketnegii <aniket.negi2112@gmail.com>
2021-10-03 12:57:35 +0530

    String Pallindrome

--- Strings/src/com/aniket/Pallindrome.java
+++ Strings/src/com/aniket/Pallindrome.java
@@ -1,4 +1,18 @@
 package com.aniket;
 
 public class Pallindrome {
+    public static void main(String[] args) {
+        String str = "abceccba";
+        System.out.println(checkPalindrome(str));
+    }
+    static boolean checkPalindrome(String s){
+        if(s == null || s.length() == 0) return true;
+        for(int i=0; i<s.length()/2;i++){
+            char start = s.charAt(i);
+            char end = s.charAt(s.length()-1 - i);
+
+            if( start != end) return false;
+        }
+        return true;
+    }
 }
