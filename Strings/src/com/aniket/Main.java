d1e97a0b5f1fc8eec4529086fa3cfbae6b644db9
aniketnegii <aniket.negi2112@gmail.com>
2021-10-03 12:50:49 +0530

    String

---
+++ Strings/src/com/aniket/Main.java
@@ -0,0 +1,43 @@
+package com.aniket;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        String name = "Aniket";
+//        String name2 = "Aniket";
+//
+//        // == check if the reference variables are pointing to same objects
+//        System.out.println(name == name2); // output: true as both name1 and name are pointing to the same POOL string
+//
+//        String a = new String("hey");
+//        String b = new String("hey");
+//
+//        System.out.println(a == b); // Output: false as they both are pointing to different objects
+//
+//        /*
+//            !!!IMPORTANT!!!!!
+//            Whenever you want to only compare the values we use the .equals method
+//        */
+//        System.out.println(a.equals(b));
+//
+//        // to get an index element of a string we use charAt( index )
+//
+//        //Placeholder for pretty printing
+//        System.out.printf("%s my name is %s.", a, name);
+//
+//        //Operators
+//        System.out.println('a' + 'b'); //ASCII value printed( char changes to ascii);
+//        System.out.println("a" + "b"); //concatenates a and b ( strings are not converted to ascii)
+//        System.out.println('a' + 1); //adds 1 to ascii value of a and prints it
+//        System.out.println( "a" + 1); //integer is converted to Integer, and it calls toString()
+
+//        //StringBuilder
+//        StringBuilder builder = new StringBuilder();
+//        for(int i=0; i<26; i++){
+//            char ch = (char)('a' + i);
+//            builder.append(ch);
+//        }
+//        System.out.println(builder);
+    }
+}
